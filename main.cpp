af6243e8b0fb7df799c39872299a6ffe6ea7937b
AlekMabry <alekmabry24@gmail.com>
2023-11-27 19:26:00 -0500

    Added custom DIO pinouts.

--- main.cpp
+++ main.cpp
@@ -6,34 +6,82 @@
 #define Wait(ts) Sleep((int)(1000*ts))
 #define GETBIT(var, bit)	(((var) >> (bit)) & 1)
 
+// Circuit inputs, AD2 outputs (number corresponds to DIO pin):
+const int S1 = 12;
+const int S0 = 11;
+const int X2 = 10;
+const int X1 = 9;
+const int X0 = 8;
+
+// Circuit outputs, AD2 inputs (number corresponds to DIO pin):
+const int NextS1 = 4;
+const int NextS0 = 3;
+const int Dispense = 2;
+const int Change1 = 1;
+const int Change0 = 0;
+
+/* Tests and input/output combination, printing test results to command line
+ * ('-' when expected bit matches the read bit, and 'F' when it does not).
+ *
+ * @param hdwf              AD2/AD3 device handle
+ * @param state             Test state:         0b[S1][S0]
+ * @param in                Test input:         0b[X2][X1][X0]
+ * @param expected_state    Expected output:    0b[NextS1][NextS0]
+ * @param expected_out      Expected output:    0b[Dispense][Change1][Change0]
+ */
 void test(HDWF hdwf, char state, char in, char expected_state, char expected_out)
 {
-    // set value on enabled IO pins
-    FDwfDigitalIOOutputSet(hdwf, (state << 11) | (in << 8));
+    // Set digital output values
+    unsigned int outputMask = 0;
+    outputMask |= (GETBIT(state, 1) << S1);
+    outputMask |= (GETBIT(state, 0) << S0);
+    outputMask |= (GETBIT(in, 2) << X2);
+    outputMask |= (GETBIT(in, 1) << X1);
+    outputMask |= (GETBIT(in, 0) << X0);
+    FDwfDigitalIOOutputSet(hdwf, outputMask);
 
+    // Wait for circuit gates to change (each gate should change in ~17ns so this may not be necessary)
     Sleep(10);
 
+    // Read circuit output (AD2 input)
     FDwfDigitalIOStatus(hdwf);
-
     unsigned int dwRead;
     FDwfDigitalIOInputStatus(hdwf, &dwRead);
-    printf("%d  %d  %d  %d  %d  | %c  %c  %c  %c  %c | %d%d%d%d%d\n",
+
+    // Output whether each case succeeded or failed
+    // Output order:    S1 S0 X2 X1 X0 | Expected NextS1 NextS0 Dispense Change1 Change0 |
+    //                  Actual NextS1 NextS0 Dispense Change1 Change0 |
+    //                  Pass/Fail NextS1 NextS0 Dispense Change1 Change0
+    printf("%d  %d  %d  %d  %d  | %d  %d  %d  %d  %d  | %d  %d  %d  %d  %d  | %c  %c  %c  %c  %c\n",
+        // Test inputs (AD2 outputs)
         GETBIT(state, 1),
         GETBIT(state, 0),
         GETBIT(in, 2),
         GETBIT(in, 1),
         GETBIT(in, 0),
-        (GETBIT(expected_state, 1) == GETBIT(dwRead, 4)) ? '-' : 'F',
-        (GETBIT(expected_state, 0) == GETBIT(dwRead, 3)) ? '-' : 'F',
-        (GETBIT(expected_out, 2) == GETBIT(dwRead, 2)) ? '-' : 'F',
-        (GETBIT(expected_out, 1) == GETBIT(dwRead, 1)) ? '-' : 'F',
-        (GETBIT(expected_out, 0) == GETBIT(dwRead, 0)) ? '-' : 'F',
-        GETBIT(dwRead, 4),
-        GETBIT(dwRead, 3),
-        GETBIT(dwRead, 2),
-        GETBIT(dwRead, 1),
-        GETBIT(dwRead, 0)
+
+        // Expected output
+        GETBIT(expected_state, 1),
+        GETBIT(expected_state, 0),
+        GETBIT(expected_out, 2),
+        GETBIT(expected_out, 1),
+        GETBIT(expected_out, 0),
+
+        // Actual output (AD2 inputs)
+        GETBIT(dwRead, NextS1),
+        GETBIT(dwRead, NextS0),
+        GETBIT(dwRead, Dispense),
+        GETBIT(dwRead, Change1),
+        GETBIT(dwRead, Change0),
+
+        // Tests
+        (GETBIT(expected_state, 1) == GETBIT(dwRead, NextS1)) ? '-' : 'F',
+        (GETBIT(expected_state, 0) == GETBIT(dwRead, NextS0)) ? '-' : 'F',
+        (GETBIT(expected_out, 2) == GETBIT(dwRead, Dispense)) ? '-' : 'F',
+        (GETBIT(expected_out, 1) == GETBIT(dwRead, Change1)) ? '-' : 'F',
+        (GETBIT(expected_out, 0) == GETBIT(dwRead, Change0)) ? '-' : 'F'
 	);
+    Sleep(100);
 }
 
 int main()
@@ -50,12 +98,17 @@ int main()
         return 0;
     }
 
-    // enable output/mask from DIO 8 to 12
-    FDwfDigitalIOOutputEnableSet(hdwf, 0x1F00);
+    // DIO enable output mask
+    unsigned int outputEnableMask = 0;
+    outputEnableMask |= (1 << S1);
+    outputEnableMask |= (1 << S0);
+    outputEnableMask |= (1 << X2);
+    outputEnableMask |= (1 << X1);
+    outputEnableMask |= (1 << X0);
+    FDwfDigitalIOOutputEnableSet(hdwf, outputEnableMask);
 
-    printf("    DIO Out    |     DIO In\n");
-    printf("12 11 10 8  7  | 4  3  2  1  0\n");
-    printf("S1 S0 X2 X1 X0 | Q1 Q0 D  C1 C0\n");
+    printf("   Test Input  |     Expected   |      Actual    |    Pass/Fail  \n");
+    printf("S1 S0 X2 X1 X0 | Q1 Q0 D  C1 C0 | Q1 Q0 D  C1 C0 | Q1 Q0 D  C1 C0\n");
     test(hdwf, 0b00, 0b000, 0b00, 0b000);
     test(hdwf, 0b01, 0b000, 0b01, 0b000);
     test(hdwf, 0b10, 0b000, 0b10, 0b000);
